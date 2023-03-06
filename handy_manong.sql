28fc444234e3f8baed75063087692c1c3a725a76
Mikoy <rapidstrike13@gmail.com>
2023-03-06 15:19:59 +0800

    db update

--- handy_manong.sql
+++ handy_manong.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2023 at 07:09 PM
+-- Generation Time: Mar 06, 2023 at 08:18 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -82,18 +82,19 @@ CREATE TABLE `tbl_finder` (
   `finder_id` int(128) NOT NULL,
   `finder_name` varchar(128) NOT NULL,
   `finder_email` varchar(128) NOT NULL,
-  `finder_password` varchar(128) NOT NULL
+  `finder_password` varchar(128) NOT NULL,
+  `avatar` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_finder`
 --
 
-INSERT INTO `tbl_finder` (`finder_id`, `finder_name`, `finder_email`, `finder_password`) VALUES
-(1, 'Myk', 'caipower09@gmail.com', '$2y$10$KW.8tCEPy.w6SwHh/03Mq.JzOsLHV2Rqg9M6s1YSXI2yh1q31T.HC'),
-(2, 'Myk', 'rapidstrike13@gmail.com', '$2y$10$jTufZC60BR9fpeGYQrTev.oV/cMOOzX4fLXyqRti6d5sQ71yV45ma'),
-(3, 'Test Finder', 'mali@gmail.com', '$2y$10$Cf57wNZGQpiV2iy.4VrMX.DkX9.0cvGkq0nUcW8MQseeUMpzHc83O'),
-(4, 'Mark', 'markescala69@gmail.com', '$2y$10$ih8LcoCIKS3oDG9G7vG3/ua1xn2pPTGyZJDMKQd4P9ZPc8MEE0xJi');
+INSERT INTO `tbl_finder` (`finder_id`, `finder_name`, `finder_email`, `finder_password`, `avatar`) VALUES
+(1, 'Myk', 'caipower09@gmail.com', '$2y$10$KW.8tCEPy.w6SwHh/03Mq.JzOsLHV2Rqg9M6s1YSXI2yh1q31T.HC', 'team-1.jpg'),
+(2, 'Myk', 'rapidstrike13@gmail.com', '$2y$10$jTufZC60BR9fpeGYQrTev.oV/cMOOzX4fLXyqRti6d5sQ71yV45ma', NULL),
+(3, 'Test Finder', 'mali@gmail.com', '$2y$10$Cf57wNZGQpiV2iy.4VrMX.DkX9.0cvGkq0nUcW8MQseeUMpzHc83O', NULL),
+(4, 'Mark', 'markescala69@gmail.com', '$2y$10$ih8LcoCIKS3oDG9G7vG3/ua1xn2pPTGyZJDMKQd4P9ZPc8MEE0xJi', NULL);
 
 -- --------------------------------------------------------
 
@@ -108,16 +109,17 @@ CREATE TABLE `tbl_provider` (
   `prov_bio` varchar(128) NOT NULL DEFAULT 'No bio.',
   `prov_category` varchar(128) NOT NULL,
   `prov_password` varchar(128) NOT NULL,
-  `prov_email` varchar(128) NOT NULL
+  `prov_email` varchar(128) NOT NULL,
+  `avatar` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_provider`
 --
 
-INSERT INTO `tbl_provider` (`id`, `prov_firstname`, `prov_lastname`, `prov_bio`, `prov_category`, `prov_password`, `prov_email`) VALUES
-(2, 'test', 'Burgos', 'No bio.', 'Carpenter', '$2y$10$2zkffBwll4.Yp4EPK1H4RO2EO6VTQe4enxxqktcRRrhnO20rV9Lq.', 'escalamykkenneth@gmail.com'),
-(3, 'Liza', 'Soberano', 'No bio.', 'Carpenter', '$2y$10$1/xT9k84KBDfhhIGxiyWle9ErrX7XiHGpXCpSVkN1vjLeCtwCz9UG', 'liza@gmail.com');
+INSERT INTO `tbl_provider` (`id`, `prov_firstname`, `prov_lastname`, `prov_bio`, `prov_category`, `prov_password`, `prov_email`, `avatar`) VALUES
+(2, 'test', 'Burgos', 'No bio.', 'Carpenter', '$2y$10$2zkffBwll4.Yp4EPK1H4RO2EO6VTQe4enxxqktcRRrhnO20rV9Lq.', 'escalamykkenneth@gmail.com', 'team-1.jpg'),
+(3, 'Liza', 'Soberano', 'No bio.', 'Carpenter', '$2y$10$1/xT9k84KBDfhhIGxiyWle9ErrX7XiHGpXCpSVkN1vjLeCtwCz9UG', 'liza@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
--- home/providers.php
+++ home/providers.php
@@ -5,6 +5,7 @@ if(empty($_SESSION['id'])){
     header("location: ../auth/signin.php?error=loginrequired");
 }
 
+
 ?>
 
 
@@ -80,34 +81,145 @@ if(empty($_SESSION['id'])){
                     <!-- Nested row for non-featured blog posts-->
                     <!-- <h2 class="card-title">Providers</h2> -->
                     <div class="row row-cols-1 row-cols-md-2 mb-5" style="overflow-x: hidden;overflow-y: auto;height:600px;">
-                        <?php
-
-                            if(isset($_POST["search"])){
+                        <?php   
+                        
+                            if(isset($_POST["search"]) || isset($_GET['pages_no'])):
+                                
                                 require_once "../includes/connect.php";
-                                $id = $_SESSION["id"];
-                                $search = $_POST["search"];
-                                $sql = "SELECT * FROM `tbl_provider` WHERE ((prov_category LIKE '%$search%') OR (prov_firstname LIKE '%$search%') OR (prov_lastname LIKE '%$search%'))"; /* add where clause here */
+                                
+                                
+                                if (isset($_GET['pages_no']) && $_GET['pages_no']!="") {
+                                    $page_no = $_GET['pages_no'];
+                                    $search = null;
+                                }else {
+                                    $page_no = 1;
+                                    $search = $_POST['search'];
+                                }
+                                    
+                                $total_records_per_page = 6;
+                                $offset = ($page_no-1) * $total_records_per_page;
+                                $previous_page = $page_no - 1;
+                                $next_page = $page_no + 1;
+                                $adjacents = "2"; 
+        
+                                $result_count = mysqli_query($conn,"SELECT COUNT(*) As total_records FROM tbl_provider");
+                                $total_records = mysqli_fetch_array($result_count);
+                                $total_records = $total_records['total_records'];
+                                $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                                $second_last = $total_no_of_pages - 1; // total page minus 1
+
+                                
+                                $sql = "SELECT * FROM `tbl_provider` WHERE ((prov_category LIKE '%$search%') OR (prov_firstname LIKE '%$search%') OR (prov_lastname LIKE '%$search%')) LIMIT $offset, $total_records_per_page"; /* add where clause here */
                                 $result = mysqli_query($conn, $sql);
-    
+                                
                                     $num = mysqli_num_rows($result); 
                                     if($num == 0) {
                                         echo "<i mb-5>No related searches to show.</i>";
+                                        echo "</div>";
+                                        
                                     }else{
                                         while($row = mysqli_fetch_array($result)){
                                             echo "<div class=\"col\">";
                                             echo "<div class=\"card mb-4\">";
-                                            echo "<a href=\"#!\"><img class=\"card-img-top\" src=\"https://dummyimage.com/700x350/dee2e6/6c757d.jpg\" alt=\"...\" /></a>";
+                                            if(empty($row['avatar'])){
+                                                echo "<a href=\"#!\"><img class=\"card-img-top\" src='../assets/images/avatar.jpg' alt=\"...\" /></a>";
+                                            }else{
+                                                echo "<a href=\"#!\"><img class=\"card-img-top\" src='../assets/images/uploads/".$row['avatar']."' alt=\"...\" /></a>";
+                                            }
                                             echo "<div class=\"card-body\">";
                                             
                                             echo "<h2 class=\"card-title h4\">".$row['prov_firstname']." ".$row['prov_lastname']."</h2>";
                                             echo "<p class=\"card-text related\">".$row['prov_bio']."</p>";
-                                            echo "<a class=\"btn btn-primary\" href=\"#\">Learn more →</a>"; /* add task id to this button to full view */
+                                            echo "<a class=\"btn btn-primary\" href='profile.php?uid=".$row['id']."'>Learn more →</a>"; /* add task id to this button to full view */
                                             echo "</div>";
                                             echo "</div>";
                                             echo "</div>";
                                         }
+                                    
+                                    
+                            ?>
+                        
+                        </div>
+                            <!-- Pagination-->
+                            <nav aria-label="Pagination">
+                                <hr class="my-0" />
+                            <ul class="pagination justify-content-center my-4">
+            
+                                <li class='page-item' <?php if($page_no <= 1){ echo "class='page-item disabled'"; } ?>>
+                                    <a class='page-link'<?php if($page_no > 1){ echo "href='?pages_no=$previous_page'"; } ?>>Previous</a>
+                                </li>
+                                
+                                <?php 
+                                    if ($total_no_of_pages <= 10){  	 
+                                        for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+                                            if ($counter == $page_no) {
+                                        echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";	
+                                                }else{
+                                        echo "<li class='page-item'><a class='page-link' href='?pages_no=$counter'>$counter</a></li>";
+                                                }
+                                        }
                                     }
-                            }else{
+                                elseif($total_no_of_pages > 10){
+                                    
+                                if($page_no <= 4) {			
+                                    for ($counter = 1; $counter < 8; $counter++){		 
+                                        if ($counter == $page_no) {
+                                            echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";	
+                                        }else{
+                                            echo "<li class='page-item'><a class='page-link' href='?pages_no=$counter'>$counter</a></li>";
+                                        }
+                                    }
+                                    echo "<li class='page-item'><a class='page-link'>...</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=$second_last'>$second_last</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                                }elseif($page_no > 4 && $page_no < $total_no_of_pages - 4) {		 
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=1'>1</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=2'>2</a></li>";
+                                    echo "<li class='page-item'><a class='page-link'>...</a></li>";
+                                    for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {			
+                                    if ($counter == $page_no) {
+                                    echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";	
+                                            }else{
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=$counter'>$counter</a></li>";
+                                            }                  
+                                }
+                                echo "<li class='page-item'><a class='page-link'>...</a></li>";
+                                echo "<li class='page-item'><a class='page-link' href='?pages_no=$second_last'>$second_last</a></li>";
+                                echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";      
+                                        }
+                                    
+                                    else{
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=1'>1</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=2'>2</a></li>";
+                                    echo "<li class='page-item'><a class='page-link'>...</a></li>";
+                            
+                                    for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+                                    if ($counter == $page_no) {
+                                    echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";	
+                                            }else{
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=$counter'>$counter</a></li>";
+                                            }                   
+                                            }
+                                        }
+                                }
+                                    
+                            ?>
+                                
+                                <li class='page-item' <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'"; } ?>>
+                                <a class='page-link' <?php if($page_no < $total_no_of_pages) { echo "href='?pages_no=$next_page'"; } ?>>Next</a>
+                                </li>
+                                <?php if($page_no < $total_no_of_pages){
+                                    echo "<li class='page-item'><a class='page-link' href='?pages_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                                    } 
+                                ?>
+                            </ul>
+                        </nav>
+                        <?php
+                                    }
+                        ?>
+
+                        <?php
+                        else:
                         require_once "../includes/connect.php";
                     
                         if (isset($_GET['page_no']) && $_GET['page_no']!="") {
@@ -116,7 +228,7 @@ if(empty($_SESSION['id'])){
                             $page_no = 1;
                         }
                             
-                        $total_records_per_page = 4;
+                        $total_records_per_page = 6;
                         $offset = ($page_no-1) * $total_records_per_page;
                         $previous_page = $page_no - 1;
                         $next_page = $page_no + 1;
@@ -127,9 +239,7 @@ if(empty($_SESSION['id'])){
                         $total_records = $total_records['total_records'];
                         $total_no_of_pages = ceil($total_records / $total_records_per_page);
                         $second_last = $total_no_of_pages - 1; // total page minus 1
-                                
-                                
-                                
+
                                 $sql = "SELECT * FROM `tbl_provider` LIMIT $offset, $total_records_per_page"; /* add where clause here */
                                 $result = mysqli_query($conn, $sql);
     
@@ -156,7 +266,6 @@ if(empty($_SESSION['id'])){
                                             echo "</div>";
                                         }
                                     }
-                            }
                         ?>
                     </div>
                     <!-- Pagination-->
@@ -221,6 +330,7 @@ if(empty($_SESSION['id'])){
                                     }
                                 }
                         }
+                            
                     ?>
                         
                         <li class='page-item' <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'"; } ?>>
@@ -228,9 +338,13 @@ if(empty($_SESSION['id'])){
                         </li>
                         <?php if($page_no < $total_no_of_pages){
                             echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
-                            } ?>
+                            } 
+                        ?>
                     </ul>
                 </nav>
+                <?php
+                    endif;
+                ?>
                 </div>
                 <!-- Side widgets-->
                 <div class="col-lg-4">
